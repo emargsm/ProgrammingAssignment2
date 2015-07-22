87858838ec46f59b525f4274f5eb13b5ab65666d
Marguerite <marguerite.smith+github@gmail.com>
2015-07-22 21:33:54 +0100

    Added more comments to each step of cachematrix.R. Is it possible to have too many comments?

--- cachematrix.R
+++ cachematrix.R
@@ -30,13 +30,32 @@
 makeCacheMatrix <- function(x = matrix()) {
   # Start with no inverse
   inverse <- NULL
+  
+  # If a matrix 'y' is passed with set(), set 
+  # the matrix 'x' to 'y'.
+  # Simultaneously, set the current 'inverse'
+  # back to NULL, since a new matrix can't
+  # have an inverse yet.
   set <- function(y) {
     x <<- y
     inverse <<- NULL
   }
+  
+  # When get() is called, simply return the
+  # current 'x'.
   get <- function() x
+  
+  # If a value 'solve' is passed with
+  # setinverse(), set the value of variable
+  # 'inverse' to 'solve'.
   setinverse <- function(solve) inverse <<- solve
+  
+  # When getinverse() is called, return the
+  # current 'inverse'.
   getinverse <- function() inverse
+  
+  # Make a list of the sub-functions, so that 
+  # they can be called with the $ format.
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -57,13 +76,36 @@ makeCacheMatrix <- function(x = matrix()) {
 ##       check to make sure the matrix is the same.
 
 cacheSolve <- function(x, ...) {
+    # First, get any stored 'inverse' from 'x'.
     inverse <- x$getinverse()
+    
+    # If it already exists, tell the user it's
+    # cached. Return the current 'inverse'.
+    # Don't do any calculation.
     if(!is.null(inverse)) {
       message("getting cached data")
       return(inverse)
     }
+    
+    # Because the 'if' above has a return statement,
+    # we can treat everything past this point as being
+    # within one big 'else'.
+    
+    # Since we know there's no current value in 'inverse',
+    # get the contents of 'x' so that we can prepare to find
+    # the actual inverse matrix.
     data <- x$get()
+    
+    # Use R's solve() function to find the inverse matrix.
+    # a %*% x = b
+    # In this case, we can just pass 'data' as 'a'. 'b'
+    # is assumed to be the identity matrix, making 'x'
+    # the inverse matrix of a.
     inverse <- solve(data, ...)
+    
+    # Now that we have the inverse, store it inside 'x'.
     x$setinverse(inverse)
+    
+    # Return the inverse matrix as well.
     inverse
 }
